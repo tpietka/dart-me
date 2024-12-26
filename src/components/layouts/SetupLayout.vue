18ca04425a6735750e6b0783315b92e9839b2010
tpietka <45951259+tpietka@users.noreply.github.com>
2024-12-26 23:32:25 +0100

    Add layout components

---
+++ src/components/layouts/SetupLayout.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+  import { useSetupStore } from "../../stores/setup";
+  const store = useSetupStore();
+</script>
+
+<template>
+  {{store.gameType}}
+  {{ $route.meta.title }}
+  <router-view/>
+</template>
