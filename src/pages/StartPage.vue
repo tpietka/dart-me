3db72407b41fce1e54533e57e2a1c385b86c9d96
tpietka <45951259+tpietka@users.noreply.github.com>
2024-12-31 19:01:19 +0100

    Add tailwind config

--- src/pages/StartPage.vue
+++ src/pages/StartPage.vue
@@ -1,9 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import BaseButton from '../components/common/BaseButton.vue';
+</script>
 
 <template>
-  <h1>
+  <h1 class="text-4xl">
     Dart Me!
   </h1>
 
-  <button @click="$router.push({name: 'Setup'})">I'm in</button>
+  <base-button class="mt-10" @click="$router.push({name: 'Setup'})">I'm in</base-button>
 </template>
