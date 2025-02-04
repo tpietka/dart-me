824edaf2faa8401310a14f819c3ba93b207bff36
tpietka <45951259+tpietka@users.noreply.github.com>
2025-02-04 21:36:15 +0100

    Add DartThrow tests

---
+++ tests/dartThrow.test.ts
@@ -0,0 +1,36 @@
+import { DartThrow } from "../src/classes/DartThrow";
+
+describe("test DartThrow class", () => {
+  it("should create a new DartThrow object", () => {
+    const dartThrow = new DartThrow();
+    expect(dartThrow).toBeTruthy();
+  });
+
+  it("should set points", () => {
+    const dartThrow = new DartThrow();
+    dartThrow.setPoints(20);
+    expect(dartThrow.points).toBe(20);
+  });
+
+  it("should set multiplier to 1 if points is greater than 20", () => {
+    const dartThrow = new DartThrow();
+    dartThrow.setMultiplier(3);
+    dartThrow.setPoints(25);
+    expect(dartThrow.getScore()).toBe(25);
+    expect(dartThrow.multiplier).toBe(1);
+  });
+
+  it("should set multiplier", () => {
+    const dartThrow = new DartThrow();
+    dartThrow.setMultiplier(2);
+    dartThrow.setPoints(19);
+    expect(dartThrow.multiplier).toBe(2);
+  });
+
+  it("should get score", () => {
+    const dartThrow = new DartThrow();
+    dartThrow.setPoints(20);
+    dartThrow.setMultiplier(2);
+    expect(dartThrow.getScore()).toBe(40);
+  });
+});
