1ebd69da8993fb3e8c89a41c3cd3b86437e8435b
Tomasz <tomasz@pietka.es>
2025-10-15 02:59:49 +0200

    Update the button classes

--- src/components/common/BaseButton.vue
+++ src/components/common/BaseButton.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 <template>
   <button
-    class="cursor-pointer bg-green-900 border-black py-3 px-8 text-xl font-bold"
+    class="!cursor-pointer !bg-green-900 !border-black !py-3 !px-8 !text-xl !font-bold"
   >
     <slot></slot>
   </button>
